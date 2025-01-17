28fe25a16226ff317e471f18788f8d7ff5013291
Pembo <kanyeze@gmail.com>
2025-01-17 18:33:28 +0100

    fix(refactor): create more user tables and run fresh migration

--- database/migrations/2025_01_17_115639_create_users_table.php
+++ database/migrations/2025_01_17_115639_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +16,11 @@ return new class extends Migration
             $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role', ['admin', 'agent', 'user'])->default('user');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
