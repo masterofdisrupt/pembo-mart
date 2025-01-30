1e343c0c820b0b0ec87e81418a777c566b371e56
Pembo <kanyeze@gmail.com>
2025-01-30 16:34:06 +0100

    refactor: update controllers, models, and config; remove obsolete views and unused uploads

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use App\Models\SMTPModel;
+use Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap();
+
     }
 }
