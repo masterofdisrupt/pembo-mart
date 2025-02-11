fa46ce21f46da76e5878c89c51cb48697992bc24
Pembo <kanyeze@gmail.com>
2025-02-11 17:46:17 +0100

    fix: update users method to fetch records dynamically and close #17

--- app/Http/Controllers/Backend/V1/AdminController.php
+++ app/Http/Controllers/Backend/V1/AdminController.php
@@ -86,12 +86,7 @@ class AdminController
     public function users(Request $request)
     {
         $data['getRecord'] = User::getRecord($request);
-        // $data['TotalAdmin'] = User::where('role', '=', 'admin')->where('is_delete', '=', 0)->count();
-        // $data['TotalAgent'] = User::where('role', '=', 'agent')->where('is_delete', '=', 0)->count();
-        // $data['TotalUser'] = User::where('role', '=', 'user')->where('is_delete', '=', 0)->count();
-        // $data['TotalActive'] = User::where('status', '=', 'active')->where('is_delete', '=', 0)->count();
-        // $data['TotalInActive'] = User::where('status', '=', 'inactive')->where('is_delete', '=', 0)->count();
-        // $data['Total'] = User::where('is_delete', '=', 0)->count();
+
         return view('backend.admin.users.list', $data);
     }
 
