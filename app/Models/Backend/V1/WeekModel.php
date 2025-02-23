68c3b311c69a8bcf858cac194d624231fc45c328
masterofdisrupt <kanyeze@gmail.com>
2025-02-23 16:37:59 +0000

    feat(V1/models): create WeekModel

---
+++ app/Models/Backend/V1/WeekModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Backend\V1;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class WeekModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'week';
+}
