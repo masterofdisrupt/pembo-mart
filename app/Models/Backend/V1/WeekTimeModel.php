2518b8a55cf04da2785e8664e545c56cdfa75893
masterofdisrupt <kanyeze@gmail.com>
2025-02-25 08:35:21 +0000

    feat(model): add model for fetching week time records with filters

---
+++ app/Models/Backend/V1/WeekTimeModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Backend\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Request;
+
+
+class WeekTimeModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'week_time';
+
+    static public function getRecordAll()
+    {
+        $return = self::select('week_time.*');
+
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('week_time.id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('name'))) {
+            $return = $return->where('week_time.name', 'like', '%' . Request::get('name') . '%');
+        }
+
+        $return = $return->get();
+
+        return $return;
+    }
+}
