38ebf48061776fe75f815e46255523d6a0777605
masterofdisrupt <kanyeze@gmail.com>
2025-03-06 03:34:51 +0000

    feat(models): add ColourModel with search functionality

---
+++ app/Models/Backend/V1/ColourModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\Backend\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Request;
+
+
+class ColourModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'colour';
+
+    static public function getRecordAll($request)
+    {
+        $return = self::select('colour.*')->orderBy('colour.id', 'desc');
+
+        // Search Start
+        if (!empty($request->id)) {
+            $return = $return->where('colour.id', '=', $request->id);
+        }
+
+        if (!empty($request->name)) {
+            $return = $return->where('colour.name', 'like', '%' . $request->name . '%');
+        }
+
+        if (!empty($request->created_at)) {
+            $return = $return->where('colour.created_at', 'like', '%' . $request->created_at . '%');
+        }
+
+        $return = $return->get();
+        return $return;
+    }
+}
