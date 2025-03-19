0c107ff3b9948a8d2a7f8c0a49ac6c9ab4e20388
masterofdisrupt <kanyeze@gmail.com>
2025-03-19 01:14:23 +0000

    fix: correct selected option logic for country dropdown in state add form

--- resources/views/backend/admin/states/add.blade.php
+++ resources/views/backend/admin/states/add.blade.php
@@ -21,10 +21,10 @@
                                 <label for="" class="col-sm-3 col-form-label">Countries Name<span
                                         style="color: red;">*</span></label>
                                 <div class="col-sm-9">
-                                    <select class="form-control" name="countries_id" required>
+                                    <select class="form-control" name="country_id" required>
                                         <option value="">Select Countries Name</option>
                                         @foreach ($getCountries as $value)
-                                            <option value="{{ $value->id }}">{{ $value->countries_name }}</option>
+                                            <option value="{{ $value->id }}">{{ $value->country_name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -33,7 +33,7 @@
                             <div class="row mb-3">
                                 <label class="col-sm-3 col-form-label">State Name <span style="color: red;">*</span></label>
                                 <div class="col-sm-9">
-                                    <input type="text" class="form-control" name="states_name"
+                                    <input type="text" class="form-control" name="state_name"
                                         placeholder="Enter State Name" required>
                                 </div>
                             </div>
