32bf2d6c479bbbacd832e76bb3cff872d4e9117d
masterofdisrupt <kanyeze@gmail.com>
2025-03-29 13:20:24 +0000

    feat(nav): add calendar link to admin sidebar menu

--- resources/views/backend/admin/body/sidebar.blade.php
+++ resources/views/backend/admin/body/sidebar.blade.php
@@ -84,13 +84,6 @@
               </li>
 
               <li class="nav-item">
-                  <a href="{{ url('admin/full_calendar') }}" class="nav-link">
-                      <i class="link-icon" data-feather="box"></i>
-                      <span class="link-title">Full Calendar</span>
-                  </a>
-              </li>
-
-              <li class="nav-item">
                   <a href="{{ url('admin/change_password') }}" class="nav-link">
                       <i class="link-icon" data-feather="box"></i>
                       <span class="link-title">Change Password</span>
@@ -202,12 +195,13 @@
                   </div>
               </li>
 
-              <li class="nav-item">
-                  <a href="pages/apps/calendar.html" class="nav-link">
+               <li class="nav-item">
+                  <a href="{{ route('fullCalendar') }}" class="nav-link">
                       <i class="link-icon" data-feather="calendar"></i>
-                      <span class="link-title">Calendar</span>
+                      <span class="link-title">Full Calendar</span>
                   </a>
               </li>
+
               <li class="nav-item nav-category">Components</li>
               <li class="nav-item">
                   <a class="nav-link" data-bs-toggle="collapse" href="#uiComponents" role="button"
