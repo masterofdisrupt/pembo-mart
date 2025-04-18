a832f81bd83ddb1d8aa77c5c9a972fc884c83801
masterofdisrupt <kanyeze@gmail.com>
2025-04-18 13:38:24 +0000

    feat(product): add product model with color and size relationships

--- app/Models/Backend/V1/ProductModel.php
+++ app/Models/Backend/V1/ProductModel.php
@@ -10,4 +10,42 @@ class ProductModel extends Model
     use HasFactory;
 
     protected $table = 'product';
+
+    protected $fillable = [
+        'title',
+        'price',
+        'product_code',
+        'description',
+        'created_by',
+        'slug'
+    ];
+
+    static public function getRecords()
+    {
+        return self::select('product.*', 'users.name as created_by_name')
+            ->join('users', 'product.created_by', '=', 'users.id')
+            ->where('product.is_delete', 0)
+            ->orderBy('product.id', 'desc')
+            ->paginate(40);
+    }
+
+    static public function getSingleRecord($id)
+    {
+        return self::where('id', $id)->first();
+    }
+
+    static public function checkSlug($slug)
+    {
+        return self::where('slug', $slug)->count();
+    }
+
+    public function getColours()
+    {
+        return $this->hasMany(ProductColoursModel::class, 'product_id', 'id');
+    }
+
+    public function getSizes()
+    {
+        return $this->hasMany(ProductSizesModel::class, 'product_id', 'id');
+    }
 }
