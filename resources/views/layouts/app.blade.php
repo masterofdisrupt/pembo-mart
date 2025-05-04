4c8bcab421cc8945d7fe06509949ac42c6372b3e
masterofdisrupt <kanyeze@gmail.com>
2025-05-04 06:26:35 +0000

    feat(security): add CSRF token protection to main layout
    - Add csrf-token meta tag
    - Enable Laravel CSRF protection
    - Support AJAX requests security

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ !empty($meta_title) ? $meta_title : '' }}</title>
     @if (!empty($meta_description))
