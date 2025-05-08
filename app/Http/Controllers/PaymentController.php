c374b4aeae29e663d14930f5c0419e9cd4061f3c
masterofdisrupt <kanyeze@gmail.com>
2025-05-08 04:16:11 +0000

    feat(payment): implement discount code validation and application

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,10 +6,64 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Models\Backend\V1\ProductModel;
 use App\Models\Backend\V1\ProductSizesModel;
+use App\Models\Backend\V1\DiscountCodeModel;
+use App\Models\Backend\V1\ShippingChargesModel;
 use Cart;
 
 class PaymentController extends Controller
 {
+        public function applyDiscount(Request $request)
+{
+    $getDiscountCode = DiscountCodeModel::CheckDiscountCode($request->discount_code);
+
+    if ($getDiscountCode) {
+        $total = Cart::getSubTotal();
+
+        if ($getDiscountCode->type == 0) { // 0 = Percentage
+            $discount = ($total * $getDiscountCode->discount_price) / 100;
+        } else { // 1 = Fixed amount
+            $discount = $getDiscountCode->discount_price;
+        }
+
+        $payable_total = max(0, $total - $discount); // Prevent negative total
+        $payable_total = number_format($payable_total, 2, '.', '');
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Discount code applied successfully.',
+            'discount' => number_format($discount, 2, '.', ''),
+            'payable_total' => $payable_total,
+            'code' => $getDiscountCode->code,
+             'status_badge' => $getDiscountCode->getStatusBadge()
+        ]);
+    } else {
+        return response()->json([
+            'success' => false,
+            'message' => 'Invalid or expired discount code.',
+            'discount' => '0.00',
+            'payable_total' => Cart::getSubTotal(),
+        ], 404);
+    }
+}
+
+        
+
+
+    public function checkout(Request $request)
+    {
+        $metaData = [
+            'meta_title' => 'Checkout',
+            'meta_description' => '',
+            'meta_keywords' => '',
+        ];
+        $shippingCharges = ShippingChargesModel::getActiveRecords();
+
+        return view('payments.checkout', [
+            'meta_title' => $metaData['meta_title'],
+            'shippingCharges' => $shippingCharges
+        ]);
+    }
+
     public function cart(Request $request)
     {
         $metaData = [
@@ -77,4 +131,5 @@ class PaymentController extends Controller
         return redirect()->back();
     }
 
+   
 }
