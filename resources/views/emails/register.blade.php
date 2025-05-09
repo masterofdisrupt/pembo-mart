2d57370dc875d2e731bf36d2721b6da662a6dc8e
masterofdisrupt <kanyeze@gmail.com>
2025-05-09 16:26:13 +0000

    fix(email): remove duplicate component closing tag in registration email

---
+++ resources/views/emails/register.blade.php
@@ -0,0 +1,26 @@
+@component('mail::message')
+Hi, {{ $user->name }}.
+
+Welcome to Pembo-Mart! We are excited to have you on board.
+
+To complete your registration, please verify your email address by clicking the link below:
+
+If you did not create an account, please ignore this email.
+
+Do not panic. Kindly, click the link below...
+
+@component('mail::button', ['url' => url('activate_email/' . base64_encode($user->id))])
+    Verify Email
+@endcomponent
+
+
+If you have any questions or need assistance, feel free to reach out to our support team.
+
+Thank you for joining us!
+
+Best regards,  
+The Team  
+Note: This is an automated message, please do not reply.
+
+Thank You
+@endcomponent
