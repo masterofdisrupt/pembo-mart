7e5273f2943d1005a206e91e11e0eb0df7e1f897
masterofdisrupt <kanyeze@gmail.com>
2025-05-09 16:27:30 +0000

    feat(email): add password reset email template

---
+++ resources/views/emails/forgot_password.blade.php
@@ -0,0 +1,18 @@
+@component('mail::message')
+# Hi {{ $user->name }},
+
+We received a request to reset your password. No worries — it happens to the best of us!
+
+Just click the button below to create a new password:
+
+@component('mail::button', ['url' => url('reset_password/' . $user->remember_token)])
+Reset Your Password
+@endcomponent
+
+If you didn’t request a password reset, you can safely ignore this email — your password will stay the same.
+
+Need help or have questions? Feel free to reach out to our support team and we’ll be happy to assist you.
+
+Thanks,  
+{{ config('app.name') }}
+@endcomponent
