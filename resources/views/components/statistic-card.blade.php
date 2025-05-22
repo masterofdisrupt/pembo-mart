fa7156697160e8d5b5a8e217d6c137495999ca45
masterofdisrupt <kanyeze@gmail.com>
2025-05-22 07:01:56 +0000

    feat(ui): add statistic card component

---
+++ resources/views/components/statistic-card.blade.php
@@ -0,0 +1,6 @@
+<div class="col-md-3">
+    <div class="box-btn">
+        <div class="stat-value">₦{{ number_format($value, 2) }}</div>
+        <div class="stat-label">{{ $label }}</div>
+    </div>
+</div>
