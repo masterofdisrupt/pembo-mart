dfdb5149b4c79b646dcfa4246a431fe5c16de12e
masterofdisrupt <kanyeze@gmail.com>
2025-05-25 16:17:06 +0000

    feat(components): create reusable statistic card component

--- app/View/Components/StatisticCard.php
+++ app/View/Components/StatisticCard.php
@@ -8,11 +8,13 @@ class StatisticCard extends Component
 {
     public $label;
     public $value;
+    public $formatted;
 
-    public function __construct($label, $value)
+    public function __construct($label, $value, $formatted = null)
     {
         $this->label = $label;
         $this->value = $value;
+        $this->formatted = $formatted ?? $value;
     }
 
     public function render()
